b038b1cf08d2797926daf097839a53e07e782c01
ShrackeR <shracker@gmail.com>
2023-06-04 12:50:51 +0530

    Added Event Schema and CRUD API

---
+++ Backend/routes/event.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+// const { getannouncement } = require('../controllers/notificationController')
+
+// controller functions
+const { allevents,getevent,createevent,markAttendance, updateevent, deleteevent} = require('../controllers/eventController')
+
+const router = express.Router()
+// const cloudinary=require('cloudinary').v2;
+
+
+// cloudinary.config({ 
+//     cloud_name: 'ddyiex0z8', 
+//     api_key: '616962189132742', 
+//     api_secret: 's_7ldYcshqnuvBz7PnYj8E6S9fI',
+//     secure: true
+//   });
+
+// login route
+router.post('/create', createevent)
+router.post('/markAttendance',markAttendance)
+router.get('/get/:id',getevent)
+router.get('/getall',allevents)
+router.put('/update/:id',updateevent)
+router.delete('/delete/:id',deleteevent)
+
+// // signup route
+// router.post('/feesupload',feesUpload)
+// router.post('/register',registerUser)
+
+
+
+// router.get('/notification', getannouncement)
+
+
+module.exports = router
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 // const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const eventRoutes = require('./routes/event')
 // const appointmentRoutes = require('./routes/appointment')
 // const healthRoutes = require('./routes/healthroutes');
 // const Workout = require('./models/workoutModel')
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 // routes
 // app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
+app.use('/api/event', eventRoutes)
 // app.use('/api/doctor', appointmentRoutes)
 // app.use('/h', healthRoutes);
 // app.use("/api/products", productRoute);
